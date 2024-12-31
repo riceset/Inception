473f5fa15010f8d211fad9612950ae8ee7d0478b
komeno <tkomeno@student.42tokyo.jp>
2024-12-31 16:29:29 +0900

    D.

--- requirements/wordpress/script.sh
+++ requirements/wordpress/script.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+
+if [ -z "$DB_NAME" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ] || [ -z "$WP_URL" ] || [ -z "$WP_TITLE" ] || [ -z "$WP_ADMIN_USER" ] || [ -z "$WP_ADMIN_PASSWORD" ] || [ -z "$WP_ADMIN_EMAIL" ]; then
+  echo "Error: Missing required environment variables."
+  exit 1
+fi
+
 cd /var/www/html
+
+# Download and install WP-CLI
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
 
-php-fpm8.2 -F
+# Download Wordpress core
+./wp-cli.phar core download --allow-root || exit 1 
+
+# Configure database connection using environment variables
+./wp-cli.phar config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost=mariadb --allow-root || exit 1
+
+# Install Wordpress using environment variables
+./wp-cli.phar core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --allow-root || exit 1
+
+# Start PHP-FPM
+exec php-fpm8.2 -F
