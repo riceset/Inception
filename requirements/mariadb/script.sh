fc56ff14a7a63740253ec698eaf71c66c1eaa12d
komeno <tkomeno@student.42tokyo.jp>
2024-12-31 16:09:02 +0900

    .

--- requirements/mariadb/script.sh
+++ requirements/mariadb/script.sh
@@ -1,4 +1,13 @@
 #!bin/bash
 
-mysql_install_db
-mysqld
+mysql_install_db 
+if [[ $? -ne 0 ]]; then
+  echo "Error running mysql_install_db. Exiting."
+  exit 1
+fi
+
+mysqld 
+if [[ $? -ne 0 ]]; then
+  echo "Error starting mysqld. Exiting."
+  exit 1
+fi
